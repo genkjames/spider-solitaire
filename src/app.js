cd2145aa3b1f33648d9b8c833904ed98602da75e
GenayaJames <genayakjames@gmail.com>
2018-03-28 22:07:54 -0400

    game visuals

---
+++ src/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  function generateCardDesign() {
+
+  }
+
+  $("#submit").on("click", function(e){
+    e.preventDefault();
+    $("#landing").css("animation", "slide 1s ease-out 0s 1 forwards");
+  });
+
+});
