8780534e3c2e4ddd46d0e2f3e99399eb31626b85
GenayaJames <genayakjames@gmail.com>
2018-04-01 15:29:17 -0400

    visuals

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   let upDeck = 0;
   let moveHistory = [];
+  const ranks = ['K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A'];
 
   class Moves {
     constructor(element, lastPlace, hideLastCard, hide) {
@@ -15,8 +16,50 @@ $(document).ready(function() {
     }
   }
 
+  function randomLetter() {
+    return ranks[Math.round(Math.random() * (ranks.length - 1))];
+  }
+
+  function randomTop() {
+    const height = $('#title-cards').height();
+    console.log(height);
+    return Math.round(Math.random() * (height -120));
+  }
+
+  function randomLeft() {
+    const width = $('#title-cards').width();
+    return Math.round(Math.random() * (width - 120));
+  }
+
+  function randomSymbol() {
+    const symbols = ['diamond', 'heart', 'spade', 'clover'];
+    return symbols[Math.round(Math.random() * (symbols.length - 1))];
+  }
+
+  function generateTitleCards() {
+    const titleCards = $('#title-cards');
+    left = randomLeft();
+    console.log("top " + randomTop());
+    console.log("left " +left);
+    const tops = [40, 180, 120, 190, 310, 342, 446, 696, 624, 141, 332, 497, 181, 385, 497, 374, 432, 268, 624, 490, 629, 24, 45, 615, 600, 19, 51, 266];
+    const lefts = [80, 20, 190, 350, 165, 675, 174, 397, 574, 630, 352, 331, 827, 865, 569, 476, 44, 528, 743, 774, 197, 356, 522, 23, 935, 741, 931, 930];
+    const degrees = [40, 60, 20, 80, 330, 80, 50, 80, 300, 150, 20, 10, 40, 280, 320, 350, 320, 40, 80, 133, 120, -50, 40, 40, 50, 109, 146, 97];
+
+    for(let i = 0; i < tops.length; i += 1) {
+      const titleCard = $(`<div class="card"></div>`);
+      const rank = $(`<p class="${randomSymbol()}">${randomLetter()}</p>`);
+      titleCard.append(rank);
+      titleCard.append(rank.clone());
+      titleCard.css('top', tops[i]);
+      titleCard.css('left', lefts[i]);
+      titleCard.css('transform', `rotate(${degrees[i]}deg)`);
+      titleCards.append(titleCard);
+    }
+  }
+
+  generateTitleCards();
+
   function makeDeck(suitName, numOfSets) {
-    const ranks = ['K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A'];
     const deck = [];
     let val = ranks.length;
 
@@ -308,16 +351,14 @@ $(document).ready(function() {
     return score;
   }
 
-  function isWinner() {
-    clearInterval(startTimer);
-    const score = gameScore();
+  function calculateScore(sc) {
     const svg = $('svg');
     svg.css('fill', '#ffffff');
-    let sc = 0;
+    sc = 0;
 
-    if (score < 1200) {
+    if (score < 1600) {
       sc = 1;
-    } else if (score < 2800) {
+    } else if (score < 4000) {
       sc = 2;
     } else {
       sc = 3;
@@ -326,19 +367,23 @@ $(document).ready(function() {
     for (let i = 0; i < sc; i += 1) {
       svg.eq(i).css('fill', '#fdff00');
     }
+  }
+
+  function isWinner() {
+    clearInterval(startTimer);
+    $('#pause').off('click');
+    const score = gameScore();
+    calculateScore(score);
     $('#game-status-container').css('display', 'block');
     $('#status').text(`Congratulations! You got a score of ${score}.`);
   }
 
   function loss() {
     const score = gameScore();
+    $('#pause').off('click');
     $('svg').css('fill', '#ffffff');
     $('#game-status-container').css('display', 'block');
     $('#status').text(`You got a score of ${score}. Better luck next time!`);
-  }
-
-  function calculateScore() {
-    console.log('hi');
   }
 
   function timer() {
@@ -393,6 +438,7 @@ $(document).ready(function() {
     distributeInitialCards();
     clickDeck();
     gameScore();
+    pauseGame();
     setTimeout(function() {
       startTimer = setInterval(timer, 1000);
     }, 1400);
@@ -408,8 +454,9 @@ $(document).ready(function() {
 
   $("#submit").on("click", function(e){
     e.preventDefault();
-    $("#landing").css("animation", "slide 1s ease-out 0s 1 forwards");
-    play();
+    $('#landing').css('animation', 'slide 2s ease-out 0s 1 forwards');
+    $('.body-cover').css('animation', 'slide 2s ease-out 0s 1 forwards');
+    setTimeout(play, 1500);
   });
 
   $('#undo').on('click', function() {
@@ -435,25 +482,28 @@ $(document).ready(function() {
     $('.card').remove();
     moveHistory = [];
     upDeck = 0;
+    $('#pause').off('click');
     restartTimer();
     play();
   });
 
   let pause = true;
-  $('#pause').on('click', function() {
-    if(pause) {
-      $(this).text("Play");
-      clearInterval(startTimer);
-      removeClicks();
-    }
-    else {
-      $(this).text("Pause");
-      startTimer = setInterval(timer, 1000);
-      clickDeck();
-      showClicks();
-    }
-    pause = !pause;
-  });
+  function pauseGame() {
+    $('#pause').on('click', function() {
+      if(pause) {
+        $(this).text("Play");
+        clearInterval(startTimer);
+        removeClicks();
+      }
+      else {
+        $(this).text("Pause");
+        startTimer = setInterval(timer, 1000);
+        clickDeck();
+        showClicks();
+      }
+      pause = !pause;
+    });
+  }
 
   $('#close').on('click', function() {
     $('#game-status-container').css('display', 'none');
